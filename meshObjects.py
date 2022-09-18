a0734f3dc8840a80eda2f3cc673cf8eb63c4077b
Yousuf Abubakr <yousufabubakr123@berkeley.edu>
2022-09-18 08:17:12 -0700

    created basic node class

--- meshObjects.py
+++ meshObjects.py
@@ -1,1 +1,11 @@
 # Defines Node and Element classes for voxel mesh generation
+
+class Node:
+
+    # Initializing instance of a node:
+    def __init__(self, x, y, z):
+        # Instance Variables:
+        self.x = x
+        self.y = y
+        self.z = z
+        
