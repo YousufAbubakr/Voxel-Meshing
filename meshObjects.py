1bbe95b5713b7f4b6372faac7bf840dc473cd850
Yousuf Abubakr <yousufabubakr123@berkeley.edu>
2022-10-06 13:25:26 -0700

    adding comments

--- meshObjects.py
+++ meshObjects.py
@@ -25,6 +25,30 @@ class Element:
         self.id = id
 
 class Mesh:
+    ''' A "Mesh" instance is an object that describes and generates
+    the meshing features and files characteristic of a voxel meshing
+    scheme.
+
+    Example 2D Voxel Mesh Case:
+    
+    1_ _ _ _2_ _ _ _5        Elements = [E1, E2, E3, E4]
+    |       |       |        E1 Local Node IDs = [1, 2, 3, 4]
+    |  E1   |  E2   |        E2 Local Node IDs = [2, 5, 4, 6]
+    |3 _ _ 4|_ _ _ _|6       E3 Local Node IDs = [3, 4, 7, 8]
+    |       |       |        E4 Local Node IDs = [4, 6, 8, 9]
+    |  E3   |  E4   |        Global Node IDs = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+    |7 _ _ 8|_ _ _ _|9       Mesh = Elements
+    
+    Input Variables:
+        l, w, t: length, width, and thickness of mesh, 
+                 where (li, wi, ti) corresponds to an (x, y, z) coordinate system
+        Nl, Nw, Nt: number of elements along the length, width, and thickness directions
+
+    Attributes (static variables):
+        l, w, t: length, width, and thickness of mesh
+        Nl, Nw, Nt: number of elements along the length, width, and thickness directions
+        lpos, wpos, tpos: linspace of discrete x, y, z positions given by l, w, t and Nl, Nw, Nt
+    '''
 
     # Initializing instance of a mesh:
     def __init__(self, l, w, t, Nl, Nw, Nt):
@@ -48,4 +72,5 @@ class Mesh:
             for w in self.wpos:
                 for t in self.tpos:
                     node = Node(l, w, t)
-                    print(node.id)
+                    print("Node ID: ", node.id)
+                    print("Node coords: ", [node.x, node.y, node.z])
